5171f91242cbfe0b9afebe7669106cedea574c20
Kira <kawoods826@gmail.com>
2020-01-13 14:19:51 -0800

    added map function to display todo list

--- reducer-todo-list/src/reducers/todo-reducer.js
+++ reducer-todo-list/src/reducers/todo-reducer.js
@@ -1,15 +1,42 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 
-export const initialState = {
-  item: "Learn about reducers",
-  completed: false,
-  id: 3892987589
+export const initialState = [
+  {
+    item: "Learn about reducers",
+    completed: false,
+    id: 3892987589
+  },
+  {
+    item: "Learn redux",
+    completed: false,
+    id: 3892987587
+  }
+];
+
+export const reducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        { id: Date.now(), item: action.payload.item, completed: false }
+      ];
+    // case "TOGGLE_COMPLETED":
+    // case "CLEAR_COMPLETED":
+    default:
+      return state;
+  }
 };
-
-export const reducer = (state = [], action) => {};
 
 export function ToDoList() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [newTodo, setNewTodo] = useState("");
+  const handleChange = event => {
+    setNewTodo(event.target.value);
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+  console.log(state);
   return (
     <>
       <form>
@@ -17,7 +44,9 @@ export function ToDoList() {
         <input type="text"></input>
         <button>Submit</button>
       </form>
-      <div className="count">{state.item}</div>
+      {state.map(todo => (
+        <p>{todo.item}</p>
+      ))}
     </>
   );
 }
